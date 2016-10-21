b417e2216c58e8724c9e8bd99b2f3b310ed910fd
Daniel Walsh <walsh.daniel@icloud.com>
2016-10-21 16:53:24 -0400

    removed duplicate file

--- VectorException.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <exception>
-
-class VectorException : public std::exception
-{
-public:
-	VectorException(const char* pMessage);
-	virtual const char* Message() const throw();
-};
